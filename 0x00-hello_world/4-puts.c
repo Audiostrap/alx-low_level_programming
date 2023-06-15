c0d9141198796ec9cd9db8ea97950e38e965160f
Audiostrap <ogwosjoy@gmail.com>
2023-06-15 14:07:59 -0700

    using put instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * The main function prints a string using the puts function.
- * It then returns 0 to indicate successful execution.
+ * main function prints a string using the puts function.
+ * returns 0 to indicate successful execution.
  */
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+int main(void) 
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
