60c03780f2f46f2122f2bae0e7d802ab9a65e02c
Audiostrap <ogwosjoy@gmail.com>
2023-06-15 15:22:52 -0700

    print size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,12 @@
  * on the computer it is piled on
  * Return: 0 if exited properly, non-zero otherwise
 */
-int main(void){
+int main(void)
+{
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long));
-printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
