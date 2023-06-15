0a2f15ec737d42f2390dca9b8a971b603f5a2110
Audiostrap <ogwosjoy@gmail.com>
2023-06-15 14:45:19 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,5 +7,6 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0); //Return value of 0 indicating successful execution
+return (0); 
+/*Return value of 0 indicating successful executin*/
 }
