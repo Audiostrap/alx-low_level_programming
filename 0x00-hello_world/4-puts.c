ad770ad6f140190d96a8dc72c140cc25b816f18c
Audiostrap <ogwosjoy@gmail.com>
2023-06-15 14:32:00 -0700

    using put instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  *
  * * return - is 0 to indicate successful execution.
  */
-int main(void);
+int main(void)
 {
+//Program logic goes here
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
