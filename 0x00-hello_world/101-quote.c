593e51d7571aa78c8266185ae6efdada815785f0
Audiostrap <ogwosjoy@gmail.com>
2023-06-15 15:43:47 -0700

    -Wall

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase and that piece of
+ * art useful\" - Dora Korpar, 2015-10-19
+ * return: 1 signifies and error
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+return (1);
+}
