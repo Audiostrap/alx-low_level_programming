03b66627894e2c2a711614dd815904403ef6c9dc
Audiostrap <ogwosjoy@gmail.com>
2023-06-16 16:31:06 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,6 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	int lastDigit = n % 10;
 	printf("Last digit of %d is ", n);
+	int lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
 		printf("and is greater than 5\n");
