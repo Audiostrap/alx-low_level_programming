9f0515b1c2766177f8a33cf1e85745989648f1dc
Audiostrap <ogwosjoy@gmail.com>
2023-06-17 04:52:22 -0700

    print alphabets in new lines

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lower case
+ * followed by new line
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	putchar('\n');
+	}
+	return (0);
+}
