b13befdf20b3c9ca3e7fcee28285ba44e29740f5
Audiostrap <ogwosjoy@gmail.com>
2023-06-17 06:17:44 -0700

    print number from base of 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 to 0
+ * followed by a new line.
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
