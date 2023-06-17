a1f18f9ef07f7e0eeab2c5dc2c61408a811492d4
Audiostrap <ogwosjoy@gmail.com>
2023-06-17 16:07:26 -0700

    print all possible combinations of 2 digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of 2 digit number
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+        for (j = i; j <= 99; j++)
+	{
+            int tens_i = i / 10;
+            int ones_i = i % 10;
+            int tens_j = j / 10;
+            int ones_j = j % 10;
+
+            putchar('0' + tens_i);
+            putchar('0' + ones_i);
+            putchar(' ');
+            putchar('0' + tens_j);
+            putchar('0' + ones_j);
+
+            if (i < 99 || j < 99)
+{
+                putchar(',');
+                putchar(' ');
+}
+}
+}
+	putchar('\n');
+
+	return (0);
+}
