30d2325c23a7e3ff3ec2a9179544c1927ac98b78
Audiostrap <ogwosjoy@gmail.com>
2023-06-17 14:40:49 -0700

    print number combinations

--- 0x01-variables_if_else_while/100-print_combo3.c
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -5,14 +5,14 @@
  * Retun: 0 (suceces):
 */
 
-int main() 
+int main()
 {
     int i, j;
 
-    for (i = 0; i <= 8; i++) 
+    for (i = 0; i <= 8; i++)
     {
         for (j = i + 1; j <= 9; j++)
-	{
+        {
             putchar('0' + i);
             putchar('0' + j);
             if (i < 8 || j < 9) {
