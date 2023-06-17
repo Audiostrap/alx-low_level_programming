2d57972ac4c4b99b1936980440aa3accb99e5dd4
Audiostrap <ogwosjoy@gmail.com>
2023-06-17 14:34:54 -0700

    number combinations

---
+++ 0x01-variables_if_else_while/100-print_combo3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of two digits
+ * 
+ * Retun: 0 (suceces):
+*/
+
+int main() 
+{
+    int i, j;
+
+    for (i = 0; i <= 8; i++) 
+    {
+        for (j = i + 1; j <= 9; j++)
+	{
+            putchar('0' + i);
+            putchar('0' + j);
+            if (i < 8 || j < 9) {
+                putchar(',');
+                putchar(' ');
+         }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
