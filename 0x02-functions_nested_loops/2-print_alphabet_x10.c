842a9c524b9718471c7aa23aba49e9795165e1a7
Audiostrap <ogwosjoy@gmail.com>
2023-06-20 17:00:37 -0700

    print 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char alphabet;
-	
+	char alphabet, letter;
+
 	for (alphabet = 0; alphabet <= 10; alphabet++)
 	{
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	_putchar(alphabet);
+	_putchar(letter);
 	}
 	_putchar('\n');
 	}
