c566077ad8ec608324a9256f931456c3ed5adb81
Audiostrap <ogwosjoy@gmail.com>
2023-06-21 13:13:42 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,12 +25,13 @@ void times_table(void)
 			{
 				_putchar('0' + result / 10);
 				_putchar('0' + result % 10);
-				_putchar(' ');
+				_putchar(',');
 			}
 			if (j < 9)
 			{
 				_putchar(' ');
 			}
 		}
+		_putchar('\n');
 	}
 }
