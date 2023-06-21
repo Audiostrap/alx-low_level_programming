4a9c407d89292f3fdaac1e40454fa8fc299ce413
Audiostrap <ogwosjoy@gmail.com>
2023-06-21 09:47:53 -0700

    unfinished 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day starting from 00:00 to 23:59
+ *
+ * print correct output
+ */
+void jack_bauer(void)
+{
+for (int hour = 0; hour < 24; hour++)
+{
+for (int minute = 0; minute < 60; minute++)
+{_putchar('0' + hour, minute);
+{
+_putchar('\n');
+return (hour);
+}
+}
+}
+}
