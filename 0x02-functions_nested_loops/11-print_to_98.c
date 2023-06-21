a4a28ca6263f311f7a5e1947539b9ef90e2b007d
Audiostrap <ogwosjoy@gmail.com>
2023-06-21 15:29:56 -0700

    print natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from n to 98 followed by a new line
  * @n: The character to be checked
@@ -7,19 +8,26 @@
  */
 void print_to_98(int n)
 {
-	int i = 0 + n;
+int i, j;
 
-	for (i = n; i <= 98; i++)
-	{
-		if (i < 0)
-		{
-			_putchar('0' + i);
-		}
-		else
-		{
-			_putchar('0' + i);
-		}
-		_putchar(',');
-		_putchar(' ');
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
 }
 }
