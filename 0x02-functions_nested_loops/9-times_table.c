1c5b64f713ac979b3d8515e3c0a9634cf5471ca2
Audiostrap <ogwosjoy@gmail.com>
2023-06-21 12:03:00 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table starting with 0
+ *
+ * Return: prints correct output
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int result = i * j;
+
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
