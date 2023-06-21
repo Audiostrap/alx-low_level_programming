b681fa06a993c44ecf3e7922338885e35d2849df
Audiostrap <ogwosjoy@gmail.com>
2023-06-20 18:05:41 -0700

    print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - ptints the sign of a number
+ * @n: The character to be checked
+ *
+ * Return: 1 and print + if n is greater than zero; 0 and print 0 if n is zero
+ *  otherwise print' -'
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+
+}
