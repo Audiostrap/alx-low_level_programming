c827af7c6da6189a1c22cc1189413a5cb0ab8613
Audiostrap <ogwosjoy@gmail.com>
2023-06-21 13:36:38 -0700

    add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *
+ * @int: The character to be cheked
+ */
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	_putchar('0' + result);
+	return (1);
+}
