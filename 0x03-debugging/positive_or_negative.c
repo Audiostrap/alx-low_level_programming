ad86d3e20c56724b6e861f170e44bed7929f5452
Audiostrap <ogwosjoy@gmail.com>
2023-06-22 13:14:04 -0700

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -10,12 +10,12 @@ if (i > 0)
 {
 printf("%d is positive\n", i);
 }
-else if (i < 0)
+else if (i == 0)
 {
-printf("%d is negative\n", i);
+printf("%d is zero\n", i);
 }
 else
 {
-printf("%d is zero\n", i);
+printf("%d is negative\n", i);
 }
 }
