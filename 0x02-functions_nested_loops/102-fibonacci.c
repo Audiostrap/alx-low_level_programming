d5e890719fea990bc1ef64e97f73dd88057447c0
Audiostrap <ogwosjoy@gmail.com>
2023-08-16 14:15:36 -0700

    list 50 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,14 +6,22 @@
  */
 int main(void)
 {
-	int i, current = 1, next, previous = 0;
+	int i = 0;
+	long j = 1,k = 2;
 
-	for (i = 1; i <= 50; i++)
+	while (i < 50)
 	{
-		next = previous + current;
-		printf("%d, ", next);
-		previous = current;
-		current = next;
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		++i;
 	}
 	printf("\n");
 	return (0);
