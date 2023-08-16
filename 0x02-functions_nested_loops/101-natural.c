f9eb41bad00826ee1f44bc6c13b27a69ca3cde72
Audiostrap <ogwosjoy@gmail.com>
2023-08-16 11:23:44 -0700

    list natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@
 /**
  * main - Lists all the natural numbers below 1024 (excluded)
  * that are multiples of 3 or 5.
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -19,6 +19,6 @@ int main(void)
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
-	printf("%d\n",sum);
+	printf("%d\n", sum);
 	return (0);
 }
