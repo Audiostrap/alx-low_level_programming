f6c04ad33e4db4e53a12f87bde8c6f3f0fc2ab92
Audiostrap <ogwosjoy@gmail.com>
2023-08-17 11:46:57 -0700

    positive and negative numbers

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* main - prints and  function that prints if integer is positive or negative
-*
-*  Return: 0 if successful
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
