f66b9c6c8399639ac14c9c7e3bcdd3be4604dc48
Audiostrap <ogwosjoy@gmail.com>
2023-08-18 10:56:29 -0700

    print numbers 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers from 0 to 9 
+ * print_numbers - prints the numbers from 0 to 9
  * followed by a new line
- *
  * Return: 1 if successful
  */
 void print_numbers(void)
 {
 	int result;
-	
+
 	for (result = 0; result <= 9; result++)
 	{
 		_putchar(result + '0');
