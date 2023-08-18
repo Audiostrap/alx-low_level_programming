0aa2268a0b16e54dc7812278b5c71c1495c90f6c
Audiostrap <ogwosjoy@gmail.com>
2023-08-18 13:28:56 -0700

    print the numbers 1-14 and repeat 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
   * more_numbers - Print 10 times the numbers since 0 up to 14
   *
@@ -5,18 +6,18 @@
   */
 void more_numbers(void)
 {
-	int x, y;
+	int x, result;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 14; y++)
+		for (result = 0; result <= 14; result++)
 		{
-			if (y > 9)
+			if (result > 9)
 			{
-				_putchar((y / 10) + '0');
+				_putchar((result / 10) + '0');
 			}
 
-			_putchar((y % 10) + '0');
+			_putchar((result % 10) + '0');
 		}
 
 		_putchar('\n');
