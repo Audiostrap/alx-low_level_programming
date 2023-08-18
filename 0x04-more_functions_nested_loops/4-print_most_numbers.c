001e5c358cfff1005d2df5ef05823d55cd027700
Audiostrap <ogwosjoy@gmail.com>
2023-08-18 11:16:24 -0700

    print 0-9 but skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers 0 -9
+ * followed by a new line but skips 2 and 4
+ *
+ *
+ * Return: The result
+ */
+void print_most_numbers(void)
+{
+	int result;
+
+	for (result = 0; result <= 9; result++)
+	{
+		if (result == '2' || result == '4')
+		{
+			continue;
+		}
+		_putchar(result + '0');
+	}
+	_putchar('\n');
+}
