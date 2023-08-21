32d57e8d8595c8079b4b3e9ae99b88f298bd3c1d
Audiostrap <ogwosjoy@gmail.com>
2023-08-21 13:34:45 -0700

    print a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts -  function that prints a string,
+ * followed by a new line, to stdout
+ * @str: an integer pointer
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (i = 0)
+	{
+		_putchar(str);
+		i++;
+	}
+	_putchar('\n');
+}
